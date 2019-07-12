77ba6b501ac9428a3bf2eeb980103b8245a483bc
Nagarajan Yadav <nagarajan.chelliya@in.ibm.com>
2019-07-12 16:59:52 +0530

    new files added

---
+++ j_test.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Hello world!
+ *
+ */
+public class j_test 
+{
+    public static void main( String[] args )
+    {
+    	
+    	for(int i=0;i<10;i++) {
+        System.out.println( "Hello World!" );
+    	}
+    	}
+}
